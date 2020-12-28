9843e6fea8ab223f52d07a74ebb025b04c6a31e5
timagr615 <timagr615@gmail.com>
2020-12-28 12:47:41 +0300

    Create project

---
+++ bot.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.executor import Executor
+
+from config import (BOT_TOKEN, LOGFILE, SKIP_UPDATES, ADMIN_ID, weatherapi_url, weatherapi_key)
+from weather.weatherapi import WeatherApiV1
+
+
+storage = MemoryStorage()
+weather = WeatherApiV1(weatherapi_url, weatherapi_key)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot, storage=storage)
+executor = Executor(dp, skip_updates=SKIP_UPDATES)
---
+++ main.py
@@ -0,0 +1,5 @@
+from bot import executor
+from handlers import handlers
+
+if __name__ == '__main__':
+    executor.start_polling()
