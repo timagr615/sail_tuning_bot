1fcc7687bba5acf19e55013bc9321e98ddc0c743
timagr615 <timagr615@gmail.com>
2020-12-28 13:41:00 +0300

    Подчищены импорты

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.executor import Executor
 
-from config import (BOT_TOKEN, LOGFILE, SKIP_UPDATES, ADMIN_ID, weatherapi_url, weatherapi_key)
+from config import (BOT_TOKEN, SKIP_UPDATES, ADMIN_ID, weatherapi_url, weatherapi_key)
 from weather.weatherapi import WeatherApiV1
 
 
