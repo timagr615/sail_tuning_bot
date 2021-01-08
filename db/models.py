99b4aa668813f844e0b08b37fcf69a08dde43c70
timagr615 <timagr615@gmail.com>
2021-01-08 18:53:15 +0300

    Добавлена модель персональной настройки

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from .database import Base
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
@@ -8,6 +9,30 @@ class User(Base):
     telegram_id = Column(Integer)
     firstname = Column(String)
     lastname = Column(String)
+    tuning = relationship('Tuning')
+
+
+class Tuning(Base):
+    __tablename__ = "tunings"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    boat = Column(String)
+    sail_firm = Column(String)
+    sail_model = Column(String)
+    place = Column(String)
+    wind = Column(Float)
+    gusts = Column(Float)
+    wave_height = Column(Float)
+    wave_length = Column(String)
+    crew_weight = Column(Float)
+    mast_rake = Column(Float)
+    prebend = Column(Integer)
+    tension = Column(Float)
+    mast_step = Column(Integer)
+    chocks = Column(String)
+    spreraders_leng = Column(Integer)
+    quality = Column(Integer)
+    description = Column(String)
 
 
 class Sail(Base):
