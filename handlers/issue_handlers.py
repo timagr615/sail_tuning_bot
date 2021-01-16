39169ed3a75c5a181a83d36039b2b07644b71f60
timagr615 <timagr615@gmail.com>
2021-01-16 20:09:46 +0300

    Добавлены хендлеры для записи вопросов и ошибок пользователей

---
+++ handlers/issue_handlers.py
@@ -0,0 +1,28 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import Message
+from bot import dp
+from db import crud
+from aiogram.dispatcher import FSMContext
+from db.database import SessionLocal
+
+
+class CreateIssue(StatesGroup):
+    issue = State()
+
+
+@dp.message_handler(commands='issue')
+async def issue(message: Message):
+    await CreateIssue.issue.set()
+    await message.answer('Введите подробное описание вопроса или ошибки и мы обязательно его рассмотрим в '
+                         'ближайшее время')
+
+
+@dp.message_handler(lambda message: message.text, state=CreateIssue.issue)
+async def process_issue(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['issue'] = message.text
+    db = SessionLocal()
+    crud.create_issue(db, data['issue'], message)
+    db.close()
+    await message.answer('Благодарим за предоставленную информацию!')
+    await state.finish()
